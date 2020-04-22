76bba52b1ca9f4421ee7fe4d02f5973d776fe5b6
mmcdermott18 <mmcderm8@mwm.fios-router.home>
2020-04-21 22:59:44 -0400

    updates

--- jquery-demo/js/main.js
+++ jquery-demo/js/main.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
+
     // run function on initial page load
     $('.square').click(function() {
-      $('.square').toggleClass('blue');
+      $('.square').toggleClass('square-circle');
+      $('.circle').toggleClass('hidden');
+      // $('.square').toggleClass('grow');
+      // $('body').css('background-color', 'black');
     });
+
     // run function on resize of the window
     $(window).resize(function() {
       resize();
     });
+
+    // run function on scroll of the window
     $(window).scroll(function() {
       scroll();
     });
+
 });
 
 function resize() {
   $('.square').addClass('purple');
 }
+
 function scroll() {
   $('.square').removeClass('purple');
 }
